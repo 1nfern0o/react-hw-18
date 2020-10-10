7b0bf18b317a87c63a696f91e5c853cb58d1e6f7
Roman <romantcvetkov@gmail.com>
2020-10-10 18:08:22 +0300

    Updates

---
+++ precache-manifest.66bdb3979bb4be28741c824eda2f5edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e8a30571b387bb7a68c399b769b5e1",
+    "url": "/react-hw-18/index.html"
+  },
+  {
+    "revision": "184c70f3826a3de8b49d",
+    "url": "/react-hw-18/static/css/main.9958e10e.chunk.css"
+  },
+  {
+    "revision": "fc8bc92f5a66d8227999",
+    "url": "/react-hw-18/static/js/2.3dad2f08.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/react-hw-18/static/js/2.3dad2f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184c70f3826a3de8b49d",
+    "url": "/react-hw-18/static/js/main.2719baf5.chunk.js"
+  },
+  {
+    "revision": "6511fa752a7d0bffb13d",
+    "url": "/react-hw-18/static/js/runtime-main.5fb73a20.js"
+  },
+  {
+    "revision": "d964e0b3c644193e5591758f01e91f68",
+    "url": "/react-hw-18/static/media/logo.d964e0b3.svg"
+  }
+]);
